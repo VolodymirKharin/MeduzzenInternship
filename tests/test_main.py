59db2f77069cc5d69a5c448416217ff2185a3951
VHile <dalvshutnik@outlook.com>
2023-02-21 22:52:59 +0200

    refactoring

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,15 +8,3 @@ async def test_health_check(ac: AsyncClient):
     response = await ac.get("/")
     assert response.status_code == 200
     assert response.json() == data
-
-
-# from httpx import AsyncClient
-#
-#
-# async def test_health_check(ac: AsyncClient):
-#     data = {"status_code": 200, "data": None, "detail": "working"}
-#
-#     async for client in ac:
-#         response = await client.get("/")
-#         assert response.status_code == 200
-#         assert response.json() == data
