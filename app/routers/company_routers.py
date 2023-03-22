ac737294e85ded4ed4a3c8be64df3fdf4c74dfe6
hile <dalvshutnik@outlook.com>
2023-03-22 03:07:34 +0200

    refactoring

--- app/routers/company_routers.py
+++ app/routers/company_routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
 from starlette import status
-
+from schemas.schemas import UserScheme
 from schemas.company_schemas import SignUpCompany, CompanyUpdateRequest, ResultCompany, Results
 from services.company_services import CompanyServices
 from db.db_connection import get_db
@@ -11,44 +11,39 @@ company_routers = APIRouter()
 
 
 @company_routers.get("/companies", tags=["Company"], response_model=Results)
-async def get_users(db: Database = Depends(get_db), current_user=Depends(get_current_user)) -> Results:
-    company_service = CompanyServices(db=db)
-    users_list = await company_service.get_companies(current_user_id=current_user.user_id)
+async def get_users(db: Database = Depends(get_db), current_user: UserScheme = Depends(get_current_user)) -> Results:
+    company_service = CompanyServices(db=db, current_user=current_user)
+    users_list = await company_service.get_companies()
     return users_list
 
 
 @company_routers.get("/company/{company_id}", tags=["Company"], response_model=ResultCompany, status_code=status.HTTP_200_OK)
-async def get_company(company_id: int, db: Database = Depends(get_db), current_user=Depends(get_current_user)) -> ResultCompany:
-    if not current_user:
-        raise HTTPException(status_code=404, detail='User does not exist')
-    company_service = CompanyServices(db=db)
-    company_db = await company_service.get_company(company_id=company_id, current_user_id=current_user.user_id)
+async def get_company(company_id: int, db: Database = Depends(get_db), current_user: UserScheme = Depends(get_current_user)) -> ResultCompany:
+    company_service = CompanyServices(db=db, current_user=current_user)
+    company_db = await company_service.get_company(company_id=company_id)
     return company_db
 
 
 @company_routers.post("/company", tags=["Company"], response_model=ResultCompany, status_code=status.HTTP_201_CREATED)
-async def create_company(company: SignUpCompany, db: Database = Depends(get_db), current_user=Depends(get_current_user)) -> ResultCompany:
-    if not current_user:
-        raise HTTPException(status_code=404, detail='User does not exist')
-    company_service = CompanyServices(db=db)
-    new_company = await company_service.create_company(company=company, current_user_id=current_user.user_id)
+async def create_company(company: SignUpCompany, db: Database = Depends(get_db), current_user: UserScheme = Depends(get_current_user)) -> ResultCompany:
+    company_service = CompanyServices(db=db, current_user=current_user)
+    new_company = await company_service.create_company(company=company)
     return new_company
 
 
 @company_routers.put("/company/{company_id}", tags=["Company"], response_model=ResultCompany, status_code=status.HTTP_200_OK)
-async def update_company(company_id: int, company: CompanyUpdateRequest, current_user=Depends(get_current_user), db: Database = Depends(get_db)) -> ResultCompany:
-    company_service = CompanyServices(db=db)
-    updated_user = await company_service.update_company(company_id=company_id, company=company, current_user_id=current_user.user_id)
+async def update_company(company_id: int, company: CompanyUpdateRequest, db: Database = Depends(get_db), current_user: UserScheme = Depends(get_current_user)) -> ResultCompany:
+    company_service = CompanyServices(db=db, current_user=current_user)
+    updated_user = await company_service.update_company(company_id=company_id, company=company)
     if not updated_user:
         raise HTTPException(status_code=404, detail='Company does not exist')
     return updated_user
 
 
 @company_routers.delete("/company/{company_id}", tags=["Company"], status_code=status.HTTP_204_NO_CONTENT)
-async def delete_company(company_id: int, db: Database = Depends(get_db), current_user=Depends(get_current_user)):
-    current_user_id = current_user.user_id
-    company_service = CompanyServices(db=db)
-    await company_service.delete_company(company_id=company_id, current_user_id=current_user_id)
+async def delete_company(company_id: int, db: Database = Depends(get_db), current_user: UserScheme = Depends(get_current_user)):
+    company_service = CompanyServices(db=db, current_user=current_user)
+    await company_service.delete_company(company_id=company_id)
 
 
 
--- app/routers/user_routers.py
+++ app/routers/user_routers.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, Depends
 from starlette import status
 
-from schemas.schemas import UserUpdateRequest, SignUpRequest, Results, ResultUser
+from schemas.schemas import UserUpdateRequest, SignUpRequest, Results, ResultUser, UserScheme
 from services.services import UserServices
 from db.db_connection import get_db
 from databases import Database
@@ -15,15 +15,15 @@ def home():
 
 
 @user_routers.get("/users", tags=["User"], response_model=Results)
-async def get_users(db: Database = Depends(get_db), current_user=Depends(get_current_user)) -> Results:
-    user_service = UserServices(db=db)
+async def get_users(db: Database = Depends(get_db), current_user: UserScheme = Depends(get_current_user)) -> Results:
+    user_service = UserServices(db=db, current_user=current_user)
     users_list = await user_service.get_users()
     return users_list
 
 
 @user_routers.get("/user", tags=["User"], response_model=ResultUser)
-async def get_user(user_id: int, db: Database = Depends(get_db), current_user=Depends(get_current_user)) -> ResultUser:
-    user_service = UserServices(db=db)
+async def get_user(user_id: int, db: Database = Depends(get_db), current_user: UserScheme = Depends(get_current_user)) -> ResultUser:
+    user_service = UserServices(db=db, current_user=current_user)
     user_db = await user_service.get_user(user_id=user_id)
     if not user_db:
         raise HTTPException(status_code=404, detail='User does not exist')
@@ -40,11 +40,8 @@ async def create_user(user: SignUpRequest, db: Database = Depends(get_db)) -> Re
 
 
 @user_routers.put("/user", tags=["User"], response_model=ResultUser, status_code=status.HTTP_200_OK)
-async def update_user(user_id: int, user: UserUpdateRequest, current_user=Depends(get_current_user), db: Database = Depends(get_db)) -> ResultUser:
-    check_user_id = current_user.user_id
-    if user_id != check_user_id:
-        raise HTTPException(status_code=403, detail="It's not your account")
-    user_service = UserServices(db=db)
+async def update_user(user_id: int, user: UserUpdateRequest, current_user: UserScheme = Depends(get_current_user), db: Database = Depends(get_db)) -> ResultUser:
+    user_service = UserServices(db=db, current_user=current_user)
     updated_user = await user_service.update_user(user_id=user_id, user=user)
     if not updated_user:
         raise HTTPException(status_code=404, detail='User does not exist')
@@ -52,11 +49,8 @@ async def update_user(user_id: int, user: UserUpdateRequest, current_user=Depend
 
 
 @user_routers.delete("/user", tags=["User"], status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(user_id: int, db: Database = Depends(get_db), current_user=Depends(get_current_user)):
-    check_user_id = current_user.user_id
-    if user_id != check_user_id:
-        raise HTTPException(status_code=403, detail="It's not your account")
-    user_service = UserServices(db=db)
+async def delete_user(user_id: int, db: Database = Depends(get_db), current_user: UserScheme = Depends(get_current_user)):
+    user_service = UserServices(db=db, current_user=current_user)
     await user_service.delete_user(user_id=user_id)
 
 
